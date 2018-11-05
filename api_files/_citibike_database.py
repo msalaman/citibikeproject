2dbe16ec3b1a11a7a1cc86de8eed76a981aa8576
Marcos Salamanca <msalaman@student04.cse.nd.edu>
2018-11-05 14:55:26 -0500

    load data

--- api_files/_citibike_database.py
+++ api_files/_citibike_database.py
@@ -4,12 +4,28 @@ import json
 class _citibike_database: 
 
 	def __init__(self):
-		self.stations = list()
+		self.stations = dict()
 
 	def load_stations(self, station_file):
 		f = open(station_file)
-		# parse json here
+		data = json.load(f)
 		f.close()
+		for station in data['stationBeanList']:
+			key = station['id']
+			temp = {}
+			temp['stationName'] = station['stationName']
+			temp['availableDocks'] = station['availableDocks']
+			temp['latitude'] = station['latitude']
+			temp['longitude'] = station['longitude']
+			temp['availableBikes'] = station['availableBikes']
+			temp['stAddress1'] = station['stAddress1']
+			temp['lastCommunicationTime'] = station['lastCommunicationTime']
+			temp['statusValue'] = station['statusValue']
+			self.stations[key] = temp			
+		print(self.stations)
+        
+	def load_station(self, station_file, sid):
+		return 0		
 
 
 if __name__ == "__main__":
