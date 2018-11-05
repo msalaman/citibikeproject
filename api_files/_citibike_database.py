f432c5277375a04001ca47180cfbaeb5e37d8452
Abigail J Lane <alane3@student04.cse.nd.edu>
2018-11-05 14:25:30 -0500

    update bike and test

--- api_files/_citibike_database.py
+++ api_files/_citibike_database.py
@@ -1,1 +1,19 @@
 class _citibike_database: 
+
+	def __init__(self):
+		self.stations = list()
+
+	def load_stations(self, station_file):
+		f = open(station_file)
+		# parse json here
+		f.close()
+
+
+if __name__ == "__main__":
+	db = _citibike_database()
+
+	db.load_stations('bikeStations.dat')
+	
+
+
+
