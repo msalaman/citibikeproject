daf0d28e03efa6d8408a67994740b0ac77d8a6ca
Abigail J Lane <alane3@student04.cse.nd.edu>
2018-11-05 14:22:55 -0500

    update teset

--- api_files/test_api.py
+++ api_files/test_api.py
@@ -1,1 +1,16 @@
 # test file
+from _citibike_database import _citibike_database
+import unittest
+
+class TestCitibikeDatabase(unittest.TestCase):
+	
+	db = _citibike_database
+
+	#def reset_data:
+
+	# def test_get_station(self):
+
+
+if __name__ == "__main__":
+	unittest.main()
+
