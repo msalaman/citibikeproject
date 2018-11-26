083f5572a067d3dde6326ca368800552e593e07c
Marcos Salamanca <msalaman@student04.cse.nd.edu>
2018-11-26 16:51:27 -0500

    stations

--- ws_files/stations.py
+++ ws_files/stations.py
@@ -5,10 +5,34 @@ import cherrypy
 from _citibike_database import _movie_database
 
 class StationsController(object): 
-	def __init(self, db=None):
-		if db is None: 
-			self.db = _citibike_database()
-		else:
-			self.db = db
+	def __init(self, bdb):
+		self.bdb=bdb
 
-		self.db.load_stations()
+
+
+	
+
+	GET(self, sid):
+		output = {'result':'success'}
+		sid = int(sid)
+		station = self.bdb.get_station(sid)
+		if station != None:
+			output['stationName'] = station['stationName']
+			output['availableDocks'] = station['availableDocks']
+			output['availableBikes'] = station['availableBikes']
+			output['latitude'] = station['latitude']
+			output['longitude'] = station['longitude']
+			output['stAddress1'] = station['stAddress1']
+			output['lastCommunicationTime'] = station['lastCommunicationTime']
+			output['statusValue'] = station['statusValue']
+		else:
+			output['result'] = 'error'
+			output['message'] = 'station not found'
+		return json.dumps(output)
+
+
+	DELETE(self, sid):
+		output = {'result':'success'}
+		sid = int(sid)
+		self.bdb.delete_station(sid)
+		return json.dumps(output)
