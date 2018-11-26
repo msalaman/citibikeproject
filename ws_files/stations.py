0896d3a613b6aa523c00b8269ec9a07c663ef844
Abigail J Lane <alane3@student04.cse.nd.edu>
2018-11-26 16:56:50 -0500

    update main

--- ws_files/stations.py
+++ ws_files/stations.py
@@ -12,3 +12,21 @@ class StationsController(object):
 			self.bdb = bdb
 
 		self.bdb.load_stations('bikeStations.dat')
+
+	# /stations/
+	def GET_STATIONS(self):
+		l = []
+		for i in self.bdb:
+
+
+			
+			genreTitle = self.mdb.get_movie(i) # get genre and title for i [name, genres]
+			temp = {}
+			temp['genres'] = genreTitle[1]
+			temp['title'] =  genreTitle[0]
+			temp['id'] = i
+			temp['img'] = self.get_poster_by_mid(int(i))
+			l.append(temp)
+
+		output = {'movies':l, 'result':'success'}
+		return json.dumps(output)
