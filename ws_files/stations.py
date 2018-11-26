16d022fe790c4230e2938ec465a0a9eae6aadac0
Abigail J Lane <alane3@student04.cse.nd.edu>
2018-11-26 14:50:04 -0500

    start stations

--- ws_files/stations.py
+++ ws_files/stations.py
@@ -2,4 +2,13 @@ import json
 import requests
 import cherrypy
 
-import _citibike_database
+from _citibike_database import _movie_database
+
+class StationsController(object): 
+	def __init(self, db=None):
+		if db is None: 
+			self.db = _citibike_database()
+		else:
+			self.db = db
+
+		self.db.load_stations()
